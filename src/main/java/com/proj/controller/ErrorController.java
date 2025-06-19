023a55bfad25684a3bafbb9caf58ecc1e0920a0e
vinyas22 <vinyasb18@gmail.com>
2025-06-19 18:58:50 +0530

    sec config updated nd jwt again and error

---
+++ src/main/java/com/proj/controller/ErrorController.java
@@ -0,0 +1,12 @@
+package com.proj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+    @RequestMapping("/error")
+    public String handleError() {
+        return "error"; // Or return a ResponseEntity
+    }
+}
