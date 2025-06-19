f0d7ab755ccd901e5d2579cbf98b9b313050198b
vinyas22 <vinyasb18@gmail.com>
2025-06-19 22:48:36 +0530

    sec config  test 6

--- src/main/java/com/proj/SecurityConfig.java
+++ src/main/java/com/proj/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/favicon.ico", "/auth/**", "/error").permitAll()
                 .anyRequest().authenticated()
             )
             .authenticationProvider(authenticationProvider())
